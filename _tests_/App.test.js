141f5ce9964e0b1b9af3f494e1001f425de0e2fb
Samuel Abiassi <samuel.abiassi@gmail.com>
2018-02-02 23:39:38 +0100

    Add logger logic, refine test comments, Stub SCSS

--- _tests_/App.test.js
+++ _tests_/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Enzyme,{ shallow } from 'enzyme'
+import Enzyme,{ shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({ adapter: new Adapter() })
@@ -7,14 +7,33 @@ Enzyme.configure({ adapter: new Adapter() })
 import App from '../src/App'
 
 describe('Testing App component', ()=>{
+
   test('Should return logger if user not logged in', ()=> {
     const app = shallow(<App/>)
     app.setState({userLoggedIn:false})
     expect(app.find('#app-logger')).toHaveLength(1)
   })
+
+  test('Changing username input in logger should change userName state', ()=> {
+    const app = mount(<App/>)
+    app.setState({userLoggedIn:false})
+    app.find('#app-logger-name').simulate('change', {target : {value : "Flora"}})
+    expect(app.state('userName')).toEqual("Flora")
+  })
+
+  test('Changing password input in logger should change userPassword state', ()=> {
+    const app = mount(<App/>)
+    app.setState({userLoggedIn:false})
+    app.find('#app-logger-password').simulate('change', {target : {value : "Banana"}})
+    expect(app.state('userPassword')).toEqual("Banana")
+  })
+
   test('Should return news feed if user is logged in', ()=> {
     const app = shallow(<App/>)
     app.setState({userLoggedIn:true})
     expect(app.find('#app-news-feed')).toHaveLength(1)
   })
+
+
+
 })
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import request from 'request';
 
 import './app.scss'
 
@@ -12,6 +13,33 @@ class App extends React.Component {
     }
   }
 
+  //LOGGER LOGIC
+  handleNameChange(e){this.setState({userName:e.target.value})}
+  handlePasswordChange(e){this.setState({userPassword:e.target.value})}
+
+  handleSubmit(){
+    console.log('click')
+    request({
+      url: "https://api.wizbii.com/v1/account/validate",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      json: true,
+      body: "username=decouverte%40wizbii.com&password=decouvertewizbii&client_id=test&grant_type=password"
+    }, (error, response, body) => {
+      console.log(response.statusCode);
+      if (error) {
+        console.log(error);
+        console.log(response.statusCode);
+      }
+      else if (!error && response.statusCode === 200) {
+        console.log(body)
+        localStorage.userToken = body["access-token"]
+      }
+    })
+  }
+
   render(){
     return (
       !this.state.userLoggedIn ? (
@@ -20,13 +48,17 @@ class App extends React.Component {
             <input
               placeholder="Nom d'utilisateur"
               type="text"
+              id='app-logger-name'
+              onChange={(e)=>{this.handleNameChange(e)}}
             />
             <input
-              placeholder="Mot de passe" 
+              placeholder="Mot de passe"
               type="text"
+              id='app-logger-password'
+              onChange={(e)=>{this.handlePasswordChange(e)}}
             />
           </div>
-          <button>Submit</button>
+          <button onClick={()=>{this.handleSubmit()}}>Submit</button>
         </div>
       ) : (
         <div id='app-news-feed'>
