1f7416605f9a3cce8a3d24bca3219c4619f2c9f4
Samuel Abiassi <samuel.abiassi@gmail.com>
2018-02-02 17:58:41 +0100

    Add test to verify if user logged in

--- _tests_/App.test.js
+++ _tests_/App.test.js
@@ -7,12 +7,9 @@ Enzyme.configure({ adapter: new Adapter() })
 import App from '../src/App'
 
 describe('Testing App component', ()=>{
-  test('Should return App component', ()=> {
+  test('Should return logger if user not logged in', ()=> {
     const app = shallow(<App/>)
-    expect(app).toHaveLength(1)
-  })
-  test('Initial counter state should be 0', ()=> {
-    const app = shallow(<App/>)
-    expect(app.find('#app-counter').text()).toBe('Count: 0')
+    app.setState({userLoggedIn:false})
+    expect(app.find('#app-logger')).toHaveLength(1)
   })
 })
--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count:0
-    };
-  }
-
-  doIncrement(){
-    this.setState({count:this.state.count+1})
+      userLoggedIn: false
+    }
   }
 
   render(){
     return (
-      <div>
-        <p>Simple React App</p>
-        <button onClick={()=>{this.doIncrement()}}>Increment</button>
-        <p id='app-counter'>Count: {this.state.count}</p>
-      </div>
+      !this.state.userLoggedIn ? (
+        <div id='app-logger'>
+        </div>
+      ) : (
+        <div id='app-news-feed'>
+        </div>
+      )
     )
   }
 }
